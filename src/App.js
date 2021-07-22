20bf7aead64176a4a806e41dcf778cb3131566e0
JasleenKaur2000 <jasleenkaur46353@gmail.com>
2021-07-22 12:10:50 +0530

    added navbar and git components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Starred from './pages/Starred';
+
 function App() {
   return (
-    <Switch>
-      <Route exact path="/">
-        This is homepage.
-      </Route>
-      <Route exact path="/starred">
-        This is starred.
-      </Route>
-      <Route>ERROR : This is 404 page.</Route>
-    </Switch>
+    <div>
+      <Navbar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/starred">
+          <Starred />
+        </Route>
+        <Route>
+          <div>Not Found</div>
+        </Route>
+      </Switch>
+    </div>
   );
 }
 
