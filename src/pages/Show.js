b9ba0134cd679df593eddf60976e59762af2a554
JasleenKaur2000 <jasleenkaur46353@gmail.com>
2021-07-23 10:38:48 +0530

    added functionality for loading and error in show pages(which are the detailed info pages for show)

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -5,16 +5,41 @@ import { getApi } from '../misc/config';
 const Show = () => {
   const { id } = useParams();
   const [show, setShow] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    getApi(`/shows/${id}?embed[]=seasons&embed[]=cast`).then(results => {
-      setShow(results);
-    });
+    let isMounted = true;
+
+    getApi(`/shows/${id}?embed[]=seasons&embed[]=cast`)
+      .then(results => {
+        setTimeout(() => {
+          if (isMounted) {
+            setShow(results);
+            setIsLoading(false);
+          }
+        }, 2000);
+      })
+      .catch(err => {
+        if (isMounted) {
+          setError(err);
+          setIsLoading(false);
+        }
+      });
+    return () => {
+      isMounted = false;
+    };
   }, [id]);
 
   // eslint-disable-next-line no-console
   console.log('show', show);
 
+  if (isLoading) {
+    return <div>Loading......</div>;
+  }
+  if (error) {
+    return <div>Error occured:{error}</div>;
+  }
   return <div>This is show page.</div>;
 };
 
